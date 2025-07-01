f06d3a6e5ad8ed96dc3fe1546817244d2b99450d
John Carl Navato <jc.navato1120@gmail.com>
2025-07-01 16:14:16 +0800

    CHATGPT esp32_smart_waste_bin.ino

--- esp32_smart_waste_bin.ino
+++ esp32_smart_waste_bin.ino
@@ -4,76 +4,145 @@
 #include <WebServer.h>
 
 // WiFi credentials
-char ssid[] = "YourWiFiSSID";
-char pass[] = "YourWiFiPassword";
+const char *ssid = "PLDTHOMEFIBRJAWS5";
+const char *password = "RVN*7Ven+s_2001";
 
-// Servo pin
-#define SERVO_PIN 12
+// === Pin definitions ===
+#define GAS_SENSOR_1      32  // G32
+#define GAS_SENSOR_2      23  // G23
+#define SERVO_1_PIN       33  // G33
+#define SERVO_2_PIN       4   // G4
+#define ECHO_1            12  // G12
+#define TRIG_1            13  // G13
+#define ECHO_2            19  // G19
+#define TRIG_2            18  // G18
+#define LCD_SDA           27  // G27
+#define LCD_SCL           14  // G14
 
-// Servo positions
-#define SERVO_CENTER 90
-#define SERVO_LEFT   45   // Biodegradable
-#define SERVO_RIGHT  135  // Non-biodegradable
+// === Constants ===
+#define SERVO_LEFT        45
+#define SERVO_RIGHT       135
+#define SERVO_CENTER      90
+#define VCC               3.3
+#define RL                10.0
+#define RS_AIR            20.0
+#define CALIBRATION_SAMPLES 50
 
-Servo wasteServo;
+Servo servo1;
+Servo servo2;
 WebServer server(80);
+
+float rsAir1 = RS_AIR;
+float rsAir2 = RS_AIR;
 
 void setup() {
   Serial.begin(115200);
-  
-  // Initialize servo
-  wasteServo.attach(SERVO_PIN);
-  wasteServo.write(SERVO_CENTER);
-  
-  // Connect to WiFi
-  WiFi.begin(ssid, pass);
+
+  pinMode(GAS_SENSOR_1, INPUT);
+  pinMode(GAS_SENSOR_2, INPUT);
+  pinMode(TRIG_1, OUTPUT);
+  pinMode(ECHO_1, INPUT);
+  pinMode(TRIG_2, OUTPUT);
+  pinMode(ECHO_2, INPUT);
+
+  servo1.attach(SERVO_1_PIN);
+  servo2.attach(SERVO_2_PIN);
+  servo1.write(SERVO_CENTER);
+  servo2.write(SERVO_CENTER);
+
+  Serial.println("Calibrating MQ-135 sensors...");
+  delay(10000);
+  rsAir1 = calibrateMQ135(GAS_SENSOR_1);
+  rsAir2 = calibrateMQ135(GAS_SENSOR_2);
+  Serial.println("Calibration done.");
+
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
-  Serial.println("WiFi connected");
-  Serial.print("ESP32 IP address: ");
+  Serial.println("WiFi connected!");
+  Serial.print("Device IP: ");
   Serial.println(WiFi.localIP());
-  
-  // Setup web server routes
+
   server.on("/", handleRoot);
-  server.on("/servo/left", handleServoLeft);
-  server.on("/servo/right", handleServoRight);
-  server.on("/servo/center", handleServoCenter);
+  server.on("/servo/rotate", handleServoRotate);
+  server.on("/air-quality", handleAirQuality);
+  server.on("/distance", handleDistance);
+  server.on("/ip", handleIP);
   server.begin();
-  
-  Serial.println("Smart Waste Bin System Ready!");
+
+  Serial.println("System Ready!");
 }
 
 void loop() {
   server.handleClient();
 }
 
-// Web server handlers
+// === Handlers ===
 void handleRoot() {
-  String html = "<html><body>";
-  html += "<h1>Smart Waste Bin Control</h1>";
-  html += "<a href='/servo/left'>Biodegradable (Left)</a><br>";
-  html += "<a href='/servo/right'>Non-biodegradable (Right)</a><br>";
-  html += "<a href='/servo/center'>Center Position</a>";
-  html += "</body></html>";
-  server.send(200, "text/html", html);
+  server.send(200, "text/plain", "ESP32 Smart Wastebin is Running");
 }
 
-void handleServoLeft() {
-  wasteServo.write(SERVO_LEFT);
-  server.send(200, "application/json", "{\"status\":\"moved_left\",\"position\":" + String(SERVO_LEFT) + "}");
-  Serial.println("Servo moved to LEFT (Biodegradable)");
+void handleIP() {
+  String ip = WiFi.localIP().toString();
+  server.send(200, "application/json", "{\"ip\":\"" + ip + "\"}");
 }
 
-void handleServoRight() {
-  wasteServo.write(SERVO_RIGHT);
-  server.send(200, "application/json", "{\"status\":\"moved_right\",\"position\":" + String(SERVO_RIGHT) + "}");
-  Serial.println("Servo moved to RIGHT (Non-biodegradable)");
+void handleServoRotate() {
+  servo1.write(SERVO_LEFT);
+  servo2.write(SERVO_RIGHT);
+  delay(1000);
+  servo1.write(SERVO_CENTER);
+  servo2.write(SERVO_CENTER);
+  server.send(200, "text/plain", "Servos rotated");
 }
 
-void handleServoCenter() {
-  wasteServo.write(SERVO_CENTER);
-  server.send(200, "application/json", "{\"status\":\"centered\",\"position\":" + String(SERVO_CENTER) + "}");
-  Serial.println("Servo moved to CENTER");
+void handleAirQuality() {
+  float gas1 = readMQ135(GAS_SENSOR_1, rsAir1);
+  float gas2 = readMQ135(GAS_SENSOR_2, rsAir2);
+  String json = "{";
+  json += "\"sensor_1\":" + String(gas1, 2) + ",";
+  json += "\"sensor_2\":" + String(gas2, 2);
+  json += "}";
+  server.send(200, "application/json", json);
+}
+
+void handleDistance() {
+  long dist1 = getDistance(TRIG_1, ECHO_1);
+  long dist2 = getDistance(TRIG_2, ECHO_2);
+  String json = "{";
+  json += "\"ultrasonic_1\":" + String(dist1) + ",";
+  json += "\"ultrasonic_2\":" + String(dist2);
+  json += "}";
+  server.send(200, "application/json", json);
+}
+
+// === Sensor Logic ===
+float calibrateMQ135(int pin) {
+  float rsSum = 0;
+  for (int i = 0; i < CALIBRATION_SAMPLES; i++) {
+    int val = analogRead(pin);
+    float voltage = val * (VCC / 4095.0);
+    rsSum += RL * (VCC - voltage) / voltage;
+    delay(100);
+  }
+  return rsSum / CALIBRATION_SAMPLES;
+}
+
+float readMQ135(int pin, float rsAir) {
+  int val = analogRead(pin);
+  float voltage = val * (VCC / 4095.0);
+  float rs = RL * (VCC - voltage) / voltage;
+  return rs / rsAir;
+}
+
+long getDistance(int trig, int echo) {
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  long duration = pulseIn(echo, HIGH);
+  return duration * 0.034 / 2;
 }
